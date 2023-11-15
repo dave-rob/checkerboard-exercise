c7c6dc5f48a60f299a708a3e5371df89974b159f
Dave <dlrobjr@gmail.com>
2023-11-15 11:32:52 -0700

    Cleaned up for loop

--- script.js
+++ script.js
@@ -15,20 +15,14 @@ for (var i = 0; i<64; i++){
 var tiles = document.querySelectorAll("div");
 
 var row = 1;
-var col = 0;
 for (var i = 0; i < tiles.length; i++) {
-  //if row is odd, want odd col red & if row is even, even col are red
-  if (row % 2 == 1 && (i + 1) % 2 == 1 || row % 2 == 0 && (i + 1) % 2 == 0) {
+  if (row % 2 == 1 && i % 2 == 0 || row % 2 == 0 && i % 2 == 1) {
     //rgba-rgb+opacity 0(transparent)-1(opaque)
     tiles[i].style.backgroundColor = "rgba(215, 95, 15, " + (1-.01*i) + ")";
   } else {
     tiles[i].style.backgroundColor = "rgba(21, 195, 155, " + (.03 * i) + ")";
   }
-
-  if (col % 7 == 0 && col != 0) {
+  if ((i+1) % 8 == 0 && i != 0) {
     row++;
-    col = 0;
-  } else {
-    col++;
   }
 }  
